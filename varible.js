a60a3b9cf252542afcc5537bc40a10e0e78b675a
JungHyun0902 <kimjh5354@gmail.com>
2021-09-08 15:04:20 +0900

    ellie JS

--- varible.js
+++ varible.js
@@ -85,4 +85,14 @@ console.log(symbol1 === symbol2);
 const gSymbol1 = Symbol.for('id');
 const gSymbol2 = Symbol.for('id');
 console.log(gSymbol1 === gSymbol2); // true 
- 
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// 5. Dynamic typing: dynamically typed language 
+let text = 'hello';
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
