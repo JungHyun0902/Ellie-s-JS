19dd4768b899aa5601aae2927adf053257bc27fa
JungHyun0902 <kimjh5354@gmail.com>
2021-09-29 01:44:36 +0900

    ellie JS

--- operator.js
+++ operator.js
@@ -20,3 +20,8 @@ console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
 const postIncrement = counter++;
 // postIncrement = counter;
 // counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+const preDecrement = --counter;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
+const postDecrement = counter--;
+console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
