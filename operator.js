892bf5f995379b448ed66f0dd46f03da5966beb7
JungHyun0902 <kimjh5354@gmail.com>
2021-10-06 17:20:39 +0900

    ellie JS

--- operator.js
+++ operator.js
@@ -40,3 +40,17 @@ console.log(10 <= 6); // less than or equal
 console.log(10 > 6); // greater than
 console.log(10 >= 6); // grater than or equal
 
+//6. Logical operators: || (or), && (and), ! (not)
+const value1 = false;
+const value2 = 4 < 2;
+
+// || (or)
+console.log(`or: ${vlaue1 || value2 || check()}`);
+
+function check() {
+  for (let i = 0; i < 10; i++) {
+    //wasting time
+    console.log('imoticon');
+  }
+  return true;
+}
