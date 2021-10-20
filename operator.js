90cbdf1de82f395c1045bcf196f1b1014df472ea
JungHyun0902 <kimjh5354@gmail.com>
2021-10-20 18:05:17 +0900

    ellie JS

--- operator.js
+++ operator.js
@@ -86,3 +86,11 @@ const ellie3 = ellie1;
 console.log(ellie1 == ellie2);
 console.log(ellie1 === ellie2);
 console.log(ellie1 === ellie3);
+
+// equality - puzzler
+console.log(0 == false );
+console.log(0 === false);
+console.log('' == false);
+console.log('' === false);
+console.log(null == undefined);
+console.log(null === undefined);
