da12666b3344104cb9e6a64e8b7ea507d6895bbc
JungHyun0902 <kimjh5354@gmail.com>
2021-11-28 16:43:03 +0900

    ellie JS

--- class.js
+++ class.js
@@ -5,3 +5,23 @@
 // JavaScript classes
 // - introduced in ES6
 // - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    // fields
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods
+  speak() {
+    console.log(`${this.name}: hello!`);
+  }
+}
+
+const ellie = new Person('ellie', 20);
+console.log(ellie.name);
+console.log(ellie.age);
+ellie.speak();
