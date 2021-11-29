b50483bacb4e1e8de71c4f59b30d99a903e6ed3d
JungHyun0902 <kimjh5354@gmail.com>
2021-11-29 18:24:47 +0900

    ellie JS

--- class.js
+++ class.js
@@ -25,3 +25,44 @@ const ellie = new Person('ellie', 20);
 console.log(ellie.name);
 console.log(ellie.age);
 ellie.speak();
+
+// practice
+class Test {
+  constructor(name, job) {
+    this.name = name;
+    this.job = job;
+  }
+  introduce() {
+    console.log(`hello! My name is ${this.name}. I'm a ${this.job}`);
+  }
+}
+
+const junghyun = new Test('junghyun', 'programmer');
+console.log(junghyun.name);
+console.log(junghyun.job);
+junghyun.introduce();
+
+// 2. Getter and setters
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    // if (value < 0) {
+    //   throw Error('age can not be negative');
+    // }
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User('steve', 'jobs', -1);
+console.log(user1.age);
+
+
