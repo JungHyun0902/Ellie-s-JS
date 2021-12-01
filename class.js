9e769df06b0c2801553a8e1bc99bdc3a2099d6b4
JungHyun0902 <kimjh5354@gmail.com>
2021-12-01 15:24:24 +0900

    ellie JS

--- class.js
+++ class.js
@@ -75,3 +75,21 @@ class Experiment {
 const experiment = new Experiment();
 console.log(experiment.publicField);
 console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+  static publisher = 'Dream coding';
+  constructor(articleNumber) {
+    this.articleNumber = articleNumber;
+  }
+
+  static printPublisher() {
+    console.log(Article.publisher);
+  }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher);
+Article.printPublisher();
